a7d8e2973dbcc5124be66ec56c439252e1aa8775
itzsweetiedabyy <llanezadeniseannebernadette_bscs@plmun.edu.ph>
2025-02-24 12:31:47 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,20 @@
+public class MethodDemo {
+
+    
+    public static void greet() {
+        System.out.println("Hello from the greet method!"); 
+    }
+
+   
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+   
+    public static void main(String[] args) {
+        greet(); 
+        
+       
+        printMessage("This is my message!.");
+    }
+}
